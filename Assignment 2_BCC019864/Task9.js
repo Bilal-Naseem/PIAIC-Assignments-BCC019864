ec2fd49e7e00a31da08347af8b28fe49e137238b
BilalNaseem <bilal.naseem.200@gmail.com>
2019-11-11 00:48:05 +0500

    PIAIC Blockchain Assignment 2 Submission

---
+++ Assignment 2_BCC019864/Task9.js
@@ -0,0 +1,12 @@
+var ch = prompt("Enter a Character : ", '');
+if(ch.length == 1)
+{
+    if(ch == 'a' || ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u')
+        alert("Input Charater is a vowel");
+    else
+        alert("Input Charater is NOT a vowel");
+}
+else
+{
+    alert("Input is not a character");
+}
